f4cc93e914c161efcb593b5c21d519894424d68e
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-19 20:28:04 -0300

    Create routes: login

--- src/routes/investorcontroller.js
+++ src/routes/investorcontroller.js
@@ -1,6 +1,7 @@
 const Investor = require('../model/investordb');
 const express = require('express');
 const generateHash = require('../utils/generatehash');
+const bcrypt = require('bcrypt');
 
 const route = express.Router();
 
@@ -19,6 +20,25 @@ route.post('/register', (req, res) => {
     })
     .catch((err) => {
         res.status(500).send(`Error to generate password hash: ${err.message}`);
+    });
+});
+
+route.post('/login', (req, res) => {
+    Investor.findOne({username: req.body.username}, (err, result) => {
+        if (err) 
+            return res.status(500).send(`Error to find investor: ${req.body.username}`);
+        if (!result)
+            return res.status(500).send('Non-existent investor');
+        
+        bcrypt.compare(req.body.password, result.password, (err, same) => {
+            if (err)
+                return res.status(500).send(`Error to validate password: ${err.message}`);
+            if (!same)
+                return res.status(500).send('Invalid password');
+
+            // TODO: integrate to MoneyPad API and get all Investor's transactions from DB
+            return res.status(200).send({output: 'You are logged', investor: req.body.username});
+        });
     });
 });
 
