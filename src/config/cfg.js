27250c8a0dd21072c0feb5d01d47962bf4ba3d35
Matheus Lopes <matheus.carvalho@picpay.com>
2022-06-19 12:43:22 -0300

    Create config package

---
+++ src/config/cfg.js
@@ -0,0 +1,8 @@
+const cfg = () => {
+    return {
+        salt: 10,
+        jwt_expires: '7d'
+    };
+};
+
+module.exports = cfg();
--- src/utils/createtoken.js
+++ src/utils/createtoken.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
+const cfg = require('../config/cfg');
 
 const createUserToken = (userId, username, email) => {
-    return jwt.sign({id: userId, username: username, email: email}, process.env.SECRET, {expiresIn: '7d'});
+    return jwt.sign({id: userId, username: username, email: email}, process.env.SECRET, {expiresIn: cfg.jwt_expires});
 };
 
 module.exports = createUserToken;
