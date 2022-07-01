59b90f94575b7725dbf017d67ba19d50e68961c4
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-01 11:05:56 -0300

    Punctual refac in auth function

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,11 +3,9 @@ const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
     const token = req.headers.token;
-    console.log('token',token);
     if (!token) res.status(401).send({output: 'Not authorized'});
     jwt.verify(token, process.env.SECRET, (err, data) => {
         if (err) res.status(401).send({output: 'Invalid token'});
-        console.log('data: ',Object.keys(data));
     });
     next();
 };
