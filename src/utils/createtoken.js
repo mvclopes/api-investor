f1b18af960acdbbd7b5cee19ae25ab6c78c022ca
Matheus Lopes <matheus.carvalho@picpay.com>
2022-07-07 19:27:01 -0300

    Punctual change in create token function

--- src/utils/createtoken.js
+++ src/utils/createtoken.js
@@ -1,8 +1,17 @@
 const jwt = require('jsonwebtoken');
 const cfg = require('../config/cfg');
 
-const createUserToken = (apikey, username, email) => {
-    return jwt.sign({apikey: apikey, username: username, email: email}, process.env.SECRET, {expiresIn: cfg.jwt_expires});
+const createUserToken = (investor) => {
+    return jwt.sign(
+        {
+            apikey: investor.apikey, 
+            fullname: investor.fullname,
+            username: investor.username, 
+            email: investor.email
+        }, 
+        process.env.SECRET, 
+        {expiresIn: cfg.jwt_expires}
+    );
 };
 
 module.exports = createUserToken;
